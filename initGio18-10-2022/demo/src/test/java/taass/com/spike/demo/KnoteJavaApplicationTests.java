c1fa8dfd02a9f4a485128bd6f6e85915276bb8c0
giovanna <gio.petrone@gmail.com>
2022-12-06 16:19:19 +0100

    firs version with MinIO

---
+++ initGio18-10-2022/demo/src/test/java/taass/com/spike/demo/KnoteJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package taass.com.spike.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnoteJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
